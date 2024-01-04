3a57cbd979657f1f8152b3579cecb1b1d444d901
bretathompson <68876276+bretathompson@users.noreply.github.com>
2024-01-03 19:14:32 -0700

    Add files via upload

--- ch5/ShoppingExperience/js/app.js
+++ ch5/ShoppingExperience/js/app.js
@@ -97,3 +97,96 @@ function updateQuantity() {
 }
 
 
+// Task 3: Create the Update Quantity Function:
+// The product quantity will now update when you click the Add to Cart button multiple times. However, when you click the up and down arrows on the number 
+// input controller to adjust the quantity, the information is not retained between pages because the cart and local storage aren't being updated. In this 
+// task, we will create a new function that will remedy the problem. We will create this new function on the app.js page since it will be applied to both 
+// the shop.html and cart.html pages.
+
+// 1. At the bottom of the app.js page, create a new function named 'updateQuantity'. The function will not have any parameters. All code for the 
+//     rest of this task will go inside the function
+// 2. In order to manipulate the quantity number inputs, we first need to grab them from our HTML. Create a new variable named 'quantityNumberInput'
+//     A. Use getElementsByClassName to grab any element with the class of quantityNumberInput
+// 3. Whenever you use getElementsByClassName, the result is placed into a collection. Because of this, we have to loop through the collection to 
+//     grab each individual input. Create a FOR loop that loops through the items stored in the quantityNumberInput variable
+// 4. Inside the loop, use addEventListener to add a 'change' event to each input. We will use the change event because it will detect if anything 
+//     has changed in the number input box. The change event will fire both when the user clicks the up or down arrow and when the user types a number in the box.
+// 5. Apply an arrow function with a parameter named 'event' to the change event listener. The rest of the code will be located inside the change event
+// 6. We don't want the value of the number input box to go higher than five since the highest number of products that a user can buy is 5. To prevent this, 
+//     do the following:
+//     A. First we need to know what the current value of the box is. Create a variable named 'quantityChanged' and assign it to the current iterated item's 
+//         value attribute (quantityNumberInput[i].value)
+//     B. Next, create an IF statement that checks if the quantityChanged variable is greater than 5. If it is, set the quantityChanged variable to 
+//         five (inside of the IF statement). This will force any number that is above 5 to become a 5 thus limiting how many items of a product the user can purchase
+// 7. Next, we need to find the selected product in the cart array so we can update it's quantity. Create a new variable named 'itemToChange' directly 
+//     below the IF statement and assign it to the following:
+//     A. Apply the find( ) method to the cart array to find the item where the id of the item is the same as the id of the item that was clicked on. 
+//     Use the following code to accomplish this: let itemToChange = cart.find( (item) => item.id == event.target.id);
+// 8. Now we can update the quantity of the item. Set the quantity of the itemToChange variable to the quantityChanged variable.
+//     A. We do have a small problem here though. The value inside the quantityChanged variable could be a string instead of a number. To force the 
+//         value to be a number, you can either use the parseInt( ) method, or you can simply place a plus symbol in front of the variable name (no spaces). 
+//         We will use the plus symbol. When finished, your code should look like this: itemToChange.quantity = +quantityChanged;
+// 9. Now that we have updated the quantity property in the cart array, we will use the cart array to update the local storage. Update the local storage 
+//     with the cart array using the setItem method. Don't forget to use JSON.stringify to turn the array of objects into a string. Also, remember that the 
+//     name of the key that is storing our information inside local storage is named CART.
+// 10. After we update the cart array and local storage, we have to call the displayOrderSummary( ) function to update the Order Summary information displayed 
+//     on the page. However, we only want to do this if the function exists. Otherwise, we will get an error that the browser can't find the function. We 
+//     will remedy this by surrounding the function call with an IF statement that checks whether the displayOrderSummary function exists. We did this in 
+//     previous exercises.
+//     A. Create an IF statement
+//     B. For the condition, check whether the displayOrderSummary function exists by using this code: typeof displayOrderSummary == "function"
+//     C. Call the displayOrderSummary function inside the IF statement
+// 11. We need to call the updateQuantity function in two places: Firstly, when the Order Summary is displayed (displayOrderSummary) on the cart.js page. 
+//     Secondly, when the shopping cart is displayed (displayCart) on the shop.js page. This way, the quantity inputs will always be up-to-date
+//     A. Add a function call to the updateQuantity function at the very top of the displayOrderSummary function on the cart.js page. We want the call to 
+//         be made at the top so the quantity will be updated before we calculate the summary and display it on the page
+//     B. Add another function call to the updateQuantity function directly below the removeFromCart function call inside the displayCart function on the 
+//         shop.js page
+
+// You should now be able to change the quantity of a product in the cart and the quantity information will be retained when you move between pages or 
+//     refresh the page.
+
+
+
+
+
+// Inside the app.js file
+
+// Task 3: Create the Update Quantity Function
+function updateQuantity() {
+    // Step 2: Grab the quantity number inputs
+    let quantityNumberInputs = document.getElementsByClassName('quantityNumberInput');
+
+    // Step 3: Loop through the quantity number inputs
+    for (let i = 0; i < quantityNumberInputs.length; i++) {
+        // Step 4: Add a 'change' event listener to each input
+        quantityNumberInputs[i].addEventListener('change', (event) => {
+            // Step 5: Arrow function with parameter 'event'
+
+            // Step 6: Prevent the value from going higher than 5
+            let quantityChanged = quantityNumberInputs[i].value;
+            if (quantityChanged > 5) {
+                quantityChanged = 5;
+            }
+
+            // Step 7: Find the selected product in the cart
+            let itemToChange = cart.find((item) => item.id == event.target.id);
+
+            // Step 8: Update the quantity of the item
+            itemToChange.quantity = +quantityChanged;
+
+            // Step 9: Update local storage with the cart array
+            localStorage.setItem('CART', JSON.stringify(cart));
+
+            // Step 10: Call the displayOrderSummary function if it exists
+            if (typeof displayOrderSummary == 'function') {
+                displayOrderSummary();
+            }
+        });
+    }
+}
+
+// Step 11: Call the updateQuantity function
+updateQuantity();
+
+
--- ch5/ShoppingExperience/js/cart.js
+++ ch5/ShoppingExperience/js/cart.js
@@ -98,6 +98,23 @@ function displayOrderSummary() {
 displayOrderSummary();
 
 
+// Task 4: Update Order Summary to Include the Quantity:
+// We have one last thing we need to do with the quantity. We need to add the quantity to the Order Summary on the cart.html page so that the total amount 
+//     due is correct.
+// 1. The calculateOrderSummary function on the cart.js page is used to calculate the subtotal, shipping, tax, and total. We need to multiply the quantity 
+//     of an item by its item price to calculate the correct subtotal. In the forEach loop within the calculateOrderSummary function, the first statement 
+//     adds the prices of each item together and assigns it to the subtotal variable. This would be a good place to add our quantity. Modify the subtotal 
+//     statement to multiply the cart item's price by the cart item's quantity and assign that to the subtotal variable using the addition assignment operator
+// 2. The Kevin's Knick Knacks site charges $1 for every item that is shipped. We need to update the shipping charge for each item that has been added 
+//     multiple times to the shopping cart. The statement directly below the one you modified in step 1 calculates the shipping cost. We need to multiply 
+//     this value by the cart item's quantity as well. Modify the shipping statement to multiply the 1 dollar amount by the cart item's quantity
+
+// The money amounts should now all be correct. Here is what the output would look like if you add 4 Squishy Caterpillar items to the shopping cart:
+// The money amounts should be:
+// Subtotal -> $39.96
+// Shipping -> $4.00
+// Sales Tax -> 2.68
+// Total -> $46.64
 
 
 
